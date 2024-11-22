3e5bf086d5fe0eb515060b81c765996faf23c2ca
CoderRayMan <ankshukray@gmail.com>
2024-11-22 08:31:21 +0530

    initial

---
+++ vaanik-serv_2/src/test/java/com/example/vaanikserv_2/VaanikServ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaanikserv_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaanikServ2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
